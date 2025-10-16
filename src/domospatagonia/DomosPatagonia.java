c0be00ab3218b8bb161ba6c60d7c1c743319682c
Martin-J-Larre <martinjlarre@gmail.com>
2025-10-16 15:04:09 -0400

    #1 Frame inicio/ presentación app

--- src/domospatagonia/DomosPatagonia.java
+++ src/domospatagonia/DomosPatagonia.java
@@ -1,20 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package domospatagonia;
+
+import javax.swing.*;
+import java.awt.*;
 
 /**
  *
  * @author martin
  */
-public class DomosPatagonia {
+public class DomosPatagonia extends JFrame{
 
-    /**
-     * @param args the command line arguments
-     */
+// Frame Inicio App DomosPatagonia
+    // Constructor
+    DomosPatagonia(){
+        // Set Frame
+        setSize(1020, 584);
+        setLocation(200, 200);
+        setLayout(null);
+        
+        // Set Image
+        ImageIcon imageInicio = new ImageIcon(ClassLoader.getSystemResource("img/inicio-img.png"));
+        JLabel image = new JLabel(imageInicio);
+        image.setBounds(0, 0, 1020, 584);
+        add(image);
+        
+        // Set Btn inicio
+        JButton inicioBtn = new JButton("Inicio");
+        inicioBtn.setBounds(435, 450, 150, 50);
+        
+        //@ Cambio de color btn no funciona en Mac investigar como hacerlo
+        
+        // inicioBtn.setBackground(Color.YELLOW);
+        // inicioBtn.setOpaque(true);
+        // inicioBtn.setBorderPainted(false);
+        // inicioBtn.setForeground(Color.YELLOW);
+ 
+
+        Font btnFont = new Font("Arial", Font.BOLD, 16);
+        inicioBtn.setFont(btnFont);
+        image.add(inicioBtn);
+        
+        
+        
+        setVisible(true);
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        new DomosPatagonia();
     }
     
 }
